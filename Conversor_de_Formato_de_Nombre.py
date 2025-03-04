b3205182da42b6ffc65d386e88a44fff78df8021
Lara <167160208+guisselllara@users.noreply.github.com>
2025-03-04 15:50:52 -0600

    Update Conversor_de_Formato_de_Nombre.py
    ya PROFE?

---
+++ Conversor_de_Formato_de_Nombre.py
@@ -0,0 +1,8 @@
+# Solicitar al usuario su nombre completo en minúsculas
+nombre = input("Ingresa tu nombre completo en minúsculas: ")
+
+# Convertir el nombre a formato de título
+nombre_formateado = nombre.title()
+
+# Mostrar el resultado
+print("Nombre en formato de título:", nombre_formateado)
